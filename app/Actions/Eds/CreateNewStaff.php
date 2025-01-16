d4348627c3b664aa350c00e92b12fad84633de62
brusnitsyn <andrew.brusnitsyn@gmail.com>
2025-01-16 20:46:45 +0900

    Добавлена проверка актуальности

--- app/Actions/Eds/CreateNewStaff.php
+++ app/Actions/Eds/CreateNewStaff.php
@@ -3,16 +3,37 @@
 namespace App\Actions\Eds;
 
 use App\Models\Staff;
+use Illuminate\Support\Carbon;
 
 class CreateNewStaff
 {
     public function create(array $data)
     {
+        // Проверка открытого ключа на актуальность
+        $now = Carbon::now();
+        $validTo = Carbon::createFromTimestampMs($data['certificate']['valid_to']);
+
+        if ($validTo->isFuture()) {
+            if ($now->diffInMonths($validTo) < 1) {
+                $data['certificate']['is_request_new'] = true;
+            } else {
+                $data['certificate']['is_request_new'] = false;
+            }
+            $data['certificate']['is_valid'] = true;
+        } else {
+            $data['certificate']['is_valid'] = false;
+        }
+
         $certification = $data['certificate'];
 
         $data['gender'] = 'slava';
 
-        $staff = Staff::create($data);
+        $staff = Staff::updateOrCreate(['snils' => $data['snils']], $data);
+
+//        if ($staff->certification->exist()) {
+//
+//            return $staff;
+//        }
 
         $staff->certification()->updateOrCreate($certification);
 
