4b1bea2aa949fd74dcb4c27733f63e635d0c938a
brusnitsyn <brusnitsyn@gmail.com>
2025-05-21 08:33:38 +0900

    Добавлена проверка актуальности закрытого ключа

--- app/Jobs/ServeValidCertificationJob.php
+++ app/Jobs/ServeValidCertificationJob.php
@@ -24,23 +24,34 @@ class ServeValidCertificationJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $certifications = Certification::all();
+        Certification::chunk(100, function ($certifications) {
+            foreach ($certifications as $certification) {
+                $validTo = Carbon::createFromTimestampMs(intval($certification->valid_to));
+                $validToCloseKey = Carbon::createFromTimestampMs(intval($certification->close_key_valid_to));
 
-        foreach ($certifications as $certification) {
-            $now = Carbon::now();
-            $validTo = Carbon::createFromTimestampMs(intval($certification->valid_to));
+                $validToData = $this->checkValid($validTo);
+                $validToCloseKeyData = $this->checkValid($validToCloseKey);
 
-            if ($validTo->isFuture()) {
-                $certification->update(['is_valid' => true]);
-                if ($now->diffInMonths($validTo) < 1)
-                    $certification->update(['is_request_new' => true]);
-                else
-                    $certification->update(['is_request_new' => false]);
-            } else {
-                $certification->update(['is_valid' => false]);
-                $certification->update(['is_request_new' => false]);
+                $certification->update([
+                    'is_valid' => $validToData['is_valid'] && $validToCloseKeyData['is_valid'],
+                    'is_request_new' => $validToData['is_request_new'],
+                    'close_key_is_valid' => $validToCloseKeyData['is_valid'],
+                    'close_key_is_request_new' => $validToCloseKeyData['is_request_new']
+                ]);
             }
+        });
+    }
 
+    private function checkValid(Carbon $date): array
+    {
+        $now = Carbon::now();
+        if ($date->isFuture()) {
+            if ($now->diffInMonths($date) < 1)
+                return ['is_valid' => true, 'is_request_new' => true];
+            else
+                return ['is_valid' => true, 'is_request_new' => false];
+        } else {
+            return ['is_valid' => false, 'is_request_new' => false];
         }
     }
 }
