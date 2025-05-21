b38298b0b9314ebe498e0fee407f979e2bb4ad60
brusnitsyn <brusnitsyn@gmail.com>
2025-05-21 09:44:05 +0900

    Обновление таймзоны

--- app/Jobs/ProcessCertificateUpload.php
+++ app/Jobs/ProcessCertificateUpload.php
@@ -127,12 +127,13 @@ class ProcessCertificateUpload implements ShouldQueue
                 $patternNew = '/\d{12}Z0/';
                 if (preg_match($patternNew, $closeKeyContent, $matches)) {
                     $dateString = $matches[0];
-                    $closeKeyValidTo = Carbon::parse($dateString)->getTimestampMs();
+                    $closeKeyValidTo = Carbon::createFromFormat('ymdHis\Z', rtrim($dateString, 0), 'UTC')->getTimestampMs();
+//                    $closeKeyValidTo = Carbon::parse($dateTime)->setTimezone(0)->timezone(config('app.timezone'))->getTimestampMs();
                     break; // Если нашли дату, выходим из цикла
                 }
                 if (preg_match($pattern, $closeKeyContent, $matches)) {
                     $dateString = $matches[0];
-                    $closeKeyValidTo = Carbon::parse($dateString)->getTimestampMs();
+                    $closeKeyValidTo = Carbon::parse($dateString)->setTimezone(0)->timezone(config('app.timezone'))->getTimestampMs();
                     break; // Если нашли дату, выходим из цикла
                 }
             } catch (\Exception $e) {
